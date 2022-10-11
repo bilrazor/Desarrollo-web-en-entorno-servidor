1f8715a0841a89b286a07c54f3c36db4cf1c8081
Billy Hanover <bhanovert@afundacion.coruna.org>
2022-10-11 14:42:04 +0200

    2.2 Ejercicio 2 completo  + /detail.php?id=<id>

---
+++ sprint2phpsql/www/mysite/detail.php
@@ -0,0 +1,29 @@
+<?php
+	$db = mysqli_connect ('localhost','root','1234','mysitedb') or die ('Fail');
+?>
+<html>
+	<body>
+	<?php
+	if (!isset($_GET['pelicula_id'])) {
+		die('No se  ha especificado una pelicula');
+}
+$pelicula_id = $_GET['pelicula_id'];
+$query =  'select * from tPeliculas where id='.$pelicula_id;
+$result = mysqli_query($db, $query) or die('Query error');
+$only_row = mysqli_fetch_array($result);
+echo '<h1>'.$only_row['titulo'].'</h1>';
+echo '<h2>'.$only_row['año'].'</h2>';
+?>
+<h3>Comentarios:</h3>
+<ul>
+	<?php
+	$query2 ='select * from tComentarios where id='.$pelicula_id;
+	$result2 = mysqli_query($db, $query2) or die ('Query error');
+	while ($row = mysqli_fetch_array($result2)) {
+		echo '<li>'.$row['comentario'].'</li>';
+}
+mysqli_close($db);
+?>
+</ul>
+</body>
+</html>
