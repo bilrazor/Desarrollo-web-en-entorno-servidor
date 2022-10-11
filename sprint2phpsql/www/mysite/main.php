55a14190e05dc64b486f114e40be3371c4c16db2
Billy Hanover <bhanovert@afundacion.coruna.org>
2022-10-11 13:39:18 +0200

    2.2 Ejercicio 2 completado

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -1,6 +1,56 @@
 <?php
 	$db = mysqli_connect('localhost','root','1234','mysitedb') or die ('fail');
 ?>
-<h1> Conexion establecida </h1>
+<html>
+<body>
+<?php
+echo'<h1 align="center"> Conexion establecida </h1>';
+echo '<table border="1" align="center">';
+echo '<tr>';
+echo	'<th>ID</th>';
+echo   	'<th>NOMBRE</th>';
+echo   	'<th>ULR_IMAGEN</th>';
+echo   	'<th>GENERO</th>';
+echo	'<th>AÑO_ESTRENO</th>';
+echo '</tr>';
 
 
+	//lanzar una query
+	$query = 'Select * from tPeliculas'  ;
+	$result = mysqli_query ($db, $query) or die('Query error');
+	// Recorrer el resultado
+?>
+
+<?php
+	while ($row = mysqli_fetch_array ($result)) {
+	echo 	'<tr>';
+	echo	'<th>';
+		echo $row[0];
+		echo '<br>';
+	echo	'</th>';
+	echo   	'<th>';
+		echo $row['nombre'];
+		echo '<br>';
+	echo	'</th>';
+	echo   	'<th>';
+		?><a href="http://localhost:8083/detail.php"><img src="<?php echo $row[2]; ?>" width='100' height='100' /></a>;<?php
+		echo '<br>';
+		
+	echo	'</th>';
+	echo	'<th>';
+		echo $row['genero'];
+		echo '<br>';
+	echo	'</th>';
+	echo	'<th>';
+		echo $row[4];
+		echo '<br>';
+	echo	'</th>';
+	echo 	'</tr>';
+}
+
+	mysqli_close($db);
+echo '</table>';
+?>	
+
+</body>
+</html>
