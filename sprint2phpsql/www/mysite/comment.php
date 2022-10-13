2f82b1df4753c6f15170958d77327f43a93a5c82
Billy Hanover <bhanovert@afundacion.coruna.org>
2022-10-13 12:31:18 +0200

    13-10-2022

--- sprint2phpsql/www/mysite/comment.php
+++ sprint2phpsql/www/mysite/comment.php
@@ -1,18 +1,19 @@
 <?php
-$db = mysqli_connect('localhost', 'root', '1234', 'web_canciones') or die('Fail');
+$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
 <html>
 <body>
 <?php
-$cancion_id = $_POST['cancion_id'];
+$pelicula_id = $_POST['pelicula_id'];
 $comentario = $_POST['new_comment'];
-$query = "INSERT INTO tComentarios(comentario, cancion_id, usuario_id)
-VALUES ('".$comentario."',".$cancion_id.",NULL)";
+$query = "INSERT INTO tComentarios(comentario, pelicula_id, usuario_id)
+VALUES ('".$comentario."',".$pelicula_id.",NULL)";
 mysqli_query($db, $query) or die('Error');
+
 echo "<p>Nuevo comentario ";
 echo mysqli_insert_id($db);
 echo " añadido</p>";
-echo "<a href='/detail.php?cancion_id=".$cancion_id."'>Volver</a>";
+echo "<a href='/detail.php?pelicula_id=".$pelicula_id."'>Volver</a>";
 mysqli_close($db);
 ?>
 </body>
--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -30,15 +30,17 @@ echo '</table>';
 	$query2 ='select * from tComentarios where id='.$pelicula_id;
 	$result2 = mysqli_query($db, $query2) or die ('Query error');
 	while ($row = mysqli_fetch_array($result2)) {
-		echo '<li>'.$row['comentario'].'</li>';
+		echo $row['comentario'];
+		echo '<br>';
 }
+?>
 <p>Deja un nuevo comentario:</p>
 <form action="/comment.php" method="post">
 <textarea rows="4" cols="50" name="new_comment"></textarea><br>
-<input type="hidden" name="cancion_id" value="<?php echo $cancion_id; ?>">
+<input type="hidden" name="pelicula_id" value="<?php echo $pelicula_id; ?>">
 <input type="submit" value="Comentar">
 </form>
-
+<?php
 mysqli_close($db);
 ?>
 </ul>
