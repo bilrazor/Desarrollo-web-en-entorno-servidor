66818b8f72193a26e915a6b9263a94903cdb6d5d
Billy Hanover <bhanovert@afundacion.coruna.org>
2022-10-17 13:04:27 +0200

    Completado ejercicio 4 y 5

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -27,22 +27,22 @@ echo '</table>';
 <h3>Comentarios:</h3>
 <ul>
 	<?php
-	$query2 ='select * from tComentarios where id='.$pelicula_id;
+	$query2 ='SELECT * FROM tComentarios WHERE pelicula_id='.$pelicula_id;
 	$result2 = mysqli_query($db, $query2) or die ('Query error');
 	while ($row = mysqli_fetch_array($result2)) {
-		echo $row['comentario'];
-		echo '<br>';
+		echo '<li>'.$row['comentario'];'</li>';
+		
 }
+mysqli_close($db);
 ?>
+</ul>
 <p>Deja un nuevo comentario:</p>
 <form action="/comment.php" method="post">
 <textarea rows="4" cols="50" name="new_comment"></textarea><br>
 <input type="hidden" name="pelicula_id" value="<?php echo $pelicula_id; ?>">
 <input type="submit" value="Comentar">
 </form>
-<?php
-mysqli_close($db);
-?>
-</ul>
+
+
 </body>
 </html>
