8dd79df18fa037e173fb9d1ececff35ca1c7a0d3
bilrazor <113417155+bilrazor@users.noreply.github.com>
2022-10-18 13:08:29 +0200

    Completado ejercicio 1 (2.3)

---
+++ sprint2python/Concurso.py
@@ -0,0 +1,16 @@
+print("Si pepe compra 10 manzanas y reparte 8, cuanto mide el sol contesta con la respuesta verdadera ​")
+
+print("a)696.340 km")
+print("b)599.340 km")
+print("a)352.340 manzanas")
+
+opcion=input("escribe tu respuesta: ")
+
+if opcion == 'a' :
+    print("Respuesta correcta ")
+elif opcion == 'b':
+    print("Respuesta incorrecta ")
+elif opcion == 'c':
+    print("Respuesta incorrecta ")
+else:
+    print("introduzca una a , b o c")
