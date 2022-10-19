2d37cc852afa070bee6b6858e5c1b9812c3273b5
bilrazor <113417155+bilrazor@users.noreply.github.com>
2022-10-19 13:34:44 +0200

    Completado ejercicio 2 (2.3)

--- sprint2python/Concurso.py
+++ sprint2python/Concurso.py
@@ -1,16 +1,84 @@
+puntos = 0
 print("Si pepe compra 10 manzanas y reparte 8, cuanto mide el sol contesta con la respuesta verdadera ​")
 
 print("a)696.340 km")
 print("b)599.340 km")
 print("c)352.340 manzanas")
 
-opcion=input("escribe tu respuesta: ")
+while(True): 
+    opcion=input("escribe tu respuesta: ")
+    if opcion == 'a' :
+        print("Respuesta correcta ")
+        puntos = puntos + 10
+        break
+    if opcion == 'b':
+        print("Respuesta incorrecta ")
+        puntos = puntos - 5
+        break
+    if opcion == 'c':
+        print("Respuesta incorrecta ")
+        puntos = puntos -5  
+        break
+      
+    
+puntuacionT = str(puntos)
+print("Su puntuación es : "+puntuacionT)
 
-if opcion == 'a' :
-    print("Respuesta correcta ")
-elif opcion == 'b':
-    print("Respuesta incorrecta ")
-elif opcion == 'c':
-    print("Respuesta incorrecta ")
-else:
-    print("introduzca una a , b o c")
+
+print(" ¿Cuál es el río más caudaloso del mundo?​")
+
+print("a)El río Nilo")
+print("b)El río Roe ")
+print("c)El río Amazonas ")
+
+while(True): 
+    opcion=input("escribe tu respuesta: ")
+    if opcion == 'a' :
+        print("Respuesta incorrecta ")
+        puntos = puntos - 5
+        break
+    if opcion == 'b':
+        print("Respuesta incorrecta ")
+        puntos = puntos - 5
+        break
+    if opcion == 'c':
+        print("Respuesta correcta ")
+        puntos = puntos + 10  
+        break
+      
+    
+
+puntuacionT = str(puntos)
+print("Su puntuación es : "+puntuacionT)
+
+print("¿Cuál es la montaña más baja en la actualidad?​")
+
+print("a)Everest")
+print("b)Kilimanjaro")
+print("c)Jingshan")
+
+while(True): 
+    opcion=input("escribe tu respuesta: ")
+    if opcion == 'a' :
+        print("Respuesta incorrecta ")
+        puntos = puntos - 5
+        break
+    if opcion == 'b':
+        print("Respuesta incorrecta ")
+        puntos = puntos - 5
+        break
+    if opcion == 'c':
+        print("Respuesta correcta ")
+        puntos = puntos + 10  
+        break
+      
+    
+
+
+puntuacionT = str(puntos)
+print("Su puntuación es : "+puntuacionT)
+
+
+
+
+
