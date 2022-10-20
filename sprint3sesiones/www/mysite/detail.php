48d31c9ee68280a80823ba2a87936f54c68d1e62
Billy Hanover <bhanovert@afundacion.coruna.org>
2022-10-20 10:51:36 +0200

    Ejercicio 1 completado (3.1)

---
+++ sprint3sesiones/www/mysite/detail.php
@@ -0,0 +1,49 @@
+<?php
+	$db = mysqli_connect ('localhost','root','1234','mysitedb') or die ('Fail');
+?>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+	<body>
+	<?php
+	if (!isset($_GET['pelicula_id'])) {
+		die('No se  ha especificado una pelicula');
+}
+$pelicula_id = $_GET['pelicula_id'];
+$query =  'select * from tPeliculas where id='.$pelicula_id;
+$result = mysqli_query($db, $query) or die('Query error');
+$only_row = mysqli_fetch_array($result);
+echo '<table border="1" align="center">';
+echo '<caption>ID SELECCIONADO</caption>';
+echo  '<tr>';
+echo '<th>'.$only_row['id'].'</th>';
+echo '<th>'.$only_row['nombre'].'</th>';
+echo '<th>'.$only_row['genero'].'</th>';
+echo '<th>';
+?> <img src="<?php echo $only_row['url_imagen']?>" width="100" hight="100"/>;<?php
+echo '</th>';
+echo '</tr>';
+echo '</table>';
+
+?>
+<h3>Comentarios:</h3>
+<ul>
+	<?php
+	$query2 ='SELECT * FROM tComentarios WHERE pelicula_id='.$pelicula_id;
+	$result2 = mysqli_query($db, $query2) or die ('Query error');
+	while ($row = mysqli_fetch_array($result2)) {
+		echo '<li>'.$row['comentario'].' '.$row['fecha'];'</li>';		
+}
+mysqli_close($db);
+?>
+</ul>
+<p>Deja un nuevo comentario:</p>
+<form action="/comment.php" method="post">
+<textarea rows="4" cols="50" name="new_comment"></textarea><br>
+<input type="hidden" name="pelicula_id" value="<?php echo $pelicula_id; ?>">
+<input type="submit" value="Comentar">
+</form>
+
+
+</body>
+</html>
---
+++ sprint3sesiones/www/mysite/main.php
@@ -0,0 +1,60 @@
+<?php
+	$db = mysqli_connect('localhost','root','1234','mysitedb') or die ('fail');
+?>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+echo'<h1 align="center"> Conexion establecida </h1>';
+echo '<table border="1" align="center">';
+echo '<tr>';
+echo	'<th id=id1>ID</th>';
+echo   	'<th id=id1>NOMBRE</th>';
+echo   	'<th id=id1>ULR_IMAGEN</th>';
+echo   	'<th id=id1>GENERO</th>';
+echo	'<th id=id1>AÑO_ESTRENO</th>';
+echo '</tr>';
+
+
+	//lanzar una query
+	$query = 'Select * from tPeliculas'  ;
+	$result = mysqli_query ($db, $query) or die('Query error');
+	// Recorrer el resultado
+?>
+
+<?php
+	while ($row = mysqli_fetch_array ($result)) {
+	echo 	'<tr>';
+	echo	'<th>';
+		echo $row[0];
+		echo '<br>';
+	echo	'</th>';
+	echo   	'<th>';
+		echo $row['nombre'];
+		echo '<br>';
+	echo	'</th>';
+	echo   	'<th>';
+		?><a href="http://localhost:8083/detail.php?pelicula_id=<?php echo $row[0] ?>">
+		 <img src="<?php echo $row[2] ?>" width='100' height='100' /></a>;<?php
+		echo '<br>';
+		
+	echo	'</th>';
+	echo	'<th>';
+		echo $row['genero'];
+		echo '<br>';
+	echo	'</th>';
+	echo	'<th>';
+		echo $row[4];
+		echo '<br>';
+	echo	'</th>';
+	echo 	'</tr>';
+}
+
+	mysqli_close($db);
+echo '</table>';
+?>	
+
+</body>
+</html>
