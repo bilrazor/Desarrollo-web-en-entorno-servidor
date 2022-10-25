4b5af4ec34bc31586aeee41a0b654fa1a050ec85
bilrazor <113417155+bilrazor@users.noreply.github.com>
2022-10-25 13:31:18 +0200

    actualizado comment

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -1,16 +1,19 @@
-<?php
 $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
 <html>
 <body>
 <?php
+ session_start();
+$user_id_a_insertar = 'NULL';
+if (!empty($_SESSION['user_id'])) {
+$user_id_a_insertar = $_SESSION['user_id'];
+}
 $pelicula_id = $_POST['pelicula_id'];
 $comentario = $_POST['new_comment'];
-$query = "INSERT INTO tComentarios(comentario, pelicula_id, usuario_id)
-VALUES ('".$comentario."',".$pelicula_id.",NULL)";
+$query = "INSERT INTO tComentarios(comentario, usuario_id, pelicula_id, fecha)
+ VALUES('".$comentario."',".$user_id_a_insertar.",".$pelicula_id.", now())";
 mysqli_query($db, $query) or die('Error');
-
-echo "<p>Nuevo comentario ";
+echo "<p>Nuevo comentario " ;
 echo mysqli_insert_id($db);
 echo " añadido</p>";
 echo "<a href='/detail.php?pelicula_id=".$pelicula_id."'>Volver</a>";
