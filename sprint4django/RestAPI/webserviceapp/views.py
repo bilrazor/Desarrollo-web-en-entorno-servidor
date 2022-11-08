223e809ce661c2a1e5017690bed70c038820c5dd
bilrazor <113417155+bilrazor@users.noreply.github.com>
2022-11-08 13:40:40 +0100

    creado devolver_peliculas

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -6,3 +6,16 @@ from django.http import HttpResponse
 
 def pagina_de_prueba(request):
 	return HttpResponse("<h1> Hola Paraguay</h1>");
+	
+	
+def devolver_peliculas(request):
+	lista = Tpeliculas.objects.all()
+	respuesta_final = []
+	for fila_sql in lista=
+		diccionario ={}
+		diccionario{'id'] = fila_sql.id
+		diccionario{'titulo'] = fila_sql.titulo
+		diccionario{'fecha'] = fila_sql.año
+		respuesta_final.append(diccionario)
+	return JsonResponse(respuesta_final, safe=False)
+		
