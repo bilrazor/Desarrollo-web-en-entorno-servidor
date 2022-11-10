2edb8ac990b3a8a0c927a2def768059353dea6b4
bilrazor <113417155+bilrazor@users.noreply.github.com>
2022-11-10 10:26:02 +0100

    " "

--- sprint4django/RestAPI/RestAPI/urls.py
+++ sprint4django/RestAPI/RestAPI/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('test',views.pagina_de_prueba),
     path('peliculas',views.devolver_peliculas),
-    path('peliculas/<int:id_solicitado>',views.devolver_pelicula_por_id)
+    path('peliculas/<int:id_solicitado>',views.devolver_pelicula_por_id),
+    path('peliculas/<int:pelicula_id>/comentarios',views.guardar_comentario)
 
 ]
