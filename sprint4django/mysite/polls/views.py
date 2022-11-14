0a466a026663e9661393620c0adcaa1b56c1daf4
bilrazor <113417155+bilrazor@users.noreply.github.com>
2022-11-14 18:19:31 +0100

    vista index completa reescrita

--- sprint4django/mysite/polls/views.py
+++ sprint4django/mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from .models import Question
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 def detail(request, question_id):
@@ -24,3 +25,7 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
