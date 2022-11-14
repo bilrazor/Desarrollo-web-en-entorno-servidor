0021351abcf42a1efa0daa80b95c4a2a0513bca2
bilrazor <113417155+bilrazor@users.noreply.github.com>
2022-11-14 13:36:04 +0100

    " "

--- sprint4django/mysite/polls/models.py
+++ sprint4django/mysite/polls/models.py
@@ -4,14 +4,7 @@ from django.utils import timezone
 # Create your models here.
 
 
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
-
+ 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -21,3 +14,10 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
