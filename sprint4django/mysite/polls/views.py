939b8ea1e15a93441ac290247ba12c3c7b2b96a2
bilrazor <113417155+bilrazor@users.noreply.github.com>
2022-11-14 18:07:06 +0100

    mas vistas que realmente hacen algo

--- sprint4django/mysite/polls/views.py
+++ sprint4django/mysite/polls/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Question
+
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -16,3 +19,13 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+ 
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+# Leave the rest of the views (detail, results, vote) unchanged
