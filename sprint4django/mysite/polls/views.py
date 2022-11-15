797b157212e797fcc3bb7d7265a998348fe0e19f
bilrazor <113417155+bilrazor@users.noreply.github.com>
2022-11-15 14:48:55 +0100

    " "

--- sprint4django/mysite/polls/views.py
+++ sprint4django/mysite/polls/views.py
@@ -12,9 +12,10 @@ from .models import Choice, Question
 
 
 
+
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -43,3 +44,8 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    
+    
+    
+    
+    
