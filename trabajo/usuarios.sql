d8604828451cb0578c2dbacab55dbdae62f1a5ca
bilrazor <113417155+bilrazor@users.noreply.github.com>
2022-11-22 13:22:32 +0100

    " "

--- trabajo/usuarios.sql
+++ trabajo/usuarios.sql
@@ -1,18 +1,18 @@
 create database restauracion;
 use restauracion;
-create table Restaurante{
+create table Restaurante(
     id int not null auto_increment,
     nombre varchar(30) not null,
     descripcion varchar(30) not null,
     direccion varchar(30) not null
-} 
+);
 
-create table Usuario {
+create table Usuario (
     id int not null auto_increment,
     nombre varchar(30) not null,
     email varchar(30) not null unique,
     contraseña varchar(9) not null
-}
+);
 
 insert into Restaurante values("tortilla","un plato muy bueno","calle la paz");
 insert into Restaurante values("mariscos","un plato muy esqusito","calle jsuticia");
