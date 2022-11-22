3cfa77d71702d53e2e640aa24d22cbd22abdf8fe
bilrazor <113417155+bilrazor@users.noreply.github.com>
2022-11-22 13:46:36 +0100

    " "

--- trabajo/usuarios.sql
+++ trabajo/usuarios.sql
@@ -14,11 +14,11 @@ create table Usuario (
     contraseña varchar(9) not null
 );
 
-insert into Restaurante (id,nombre,descripcion,direccion)VALUES("tortilla","un plato muy bueno","calle la paz");
-insert into Restaurante (id,nombre,descripcion,direccion)VALUES("mariscos","un plato muy esqusito","calle jsuticia");
-insert into Restaurante (id,nombre,descripcion,direccion)VALUES("tortilla","un plato muy bueno","calle gomez");
+insert into Restaurante (nombre,descripcion,direccion)VALUES("tortilla","un plato muy bueno","calle la paz");
+insert into Restaurante (nombre,descripcion,direccion)VALUES("mariscos","un plato muy esqusito","calle jsuticia");
+insert into Restaurante (nombre,descripcion,direccion)VALUES("tortilla","un plato muy bueno","calle gomez");
 
 
-insert into Usuario (id,nombre, email,contraseña)VALUES("martin","martin@gmail.com","pepapig");
-insert into Usuario (id,nombre, email,contraseña)VALUES("pepe","pepe@gmail.com","pepapig");
-insert into Usuario (id,nombre, email,contraseña)VALUES("carl","carl@gmail.com","pepapig");
+insert into Usuario (nombre, email,contraseña)VALUES("martin","martin@gmail.com","pepapig");
+insert into Usuario (nombre, email,contraseña)VALUES("pepe","pepe@gmail.com","pepapig");
+insert into Usuario (nombre, email,contraseña)VALUES("carl","carl@gmail.com","pepapig");
